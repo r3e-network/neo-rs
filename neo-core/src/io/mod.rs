94c36b32851ab172f5e685a17354f7522754d2cd
Jimmy <jimmy@r3e.network>
2026-05-25 15:28:51 +0800

    refactor io without legacy binary reader

--- neo-core/src/io/mod.rs
+++ neo-core/src/io/mod.rs
@@ -2,11 +2,6 @@
 //!
 //! This module mirrors the C# Neo.IO namespace by re-exporting the shared `neo-io` crate.
 
-#[allow(dead_code)]
-mod binary_reader;
-
-#[allow(unused_imports)]
-pub use binary_reader::BinaryReader;
 #[allow(unused_imports)]
 pub use neo_io_crate::{
     serializable::{self, helper},
